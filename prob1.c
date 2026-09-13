90776c9a86831848de66389e8af973cbcfcb5d90
Maci Gaona <mgaona01@uafs.edu>
2026-09-12 20:11:01 -0500

    Final Update

--- prob1.c
+++ prob1.c
@@ -6,8 +6,51 @@
 
 // *** Function Protoypes ***
 char *newString(char *str, int size);
-char showArray(char **strings, int count);
+void showArray(char **strings, int count);
 
 int main(int argc, char *argv[]){
  
+  char *strings[SIZE];
+  char temp[SIZE];
+  int count = 0;
+
+  while(fgets(temp, SIZE, stdin)){
+
+    strings[count] = newString(temp, strlen(temp) + 1);
+    count++;
+  }
+
+  showArray(strings, count);
+
+  for(int i = 0; i < count; i++){
+    free(strings[i]);
+  }
+
+  return 0;
+}
+
+char *newString(char *str, int size){
+
+    char *newStr;
+    newStr = malloc(size * sizeof(char));
+    int index;
+
+    for(index = 0; index < size; index++){
+        newStr[index] = str[index];
+    }
+
+    return newStr;
+}
+
+void showArray(char **strings, int count){
+
+    int i;
+    int j;
+    for(i = 0; i < count; i++){
+        
+        for(j = 0; j < strlen(strings[i]); j++){
+
+            printf("%c", strings[i][j]);
+        }
+    }
 }
--- prob2.c
+++ prob2.c
@@ -6,9 +6,54 @@
 
 // *** Function Protoypes ***
 char *newString(char *str, int size);
-char showArray(char **strings);
+void showArray(char **strings);
 
 int main(int argc, char *argv[]){
-  
+
+  char **strings;
+  strings = malloc(SIZE * sizeof(char *));
+  char temp[SIZE];
+  int count = 0;
+
+  while(fgets(temp, SIZE, stdin)){
+
+    strings[count] = newString(temp, strlen(temp) + 1);
+    count++;
+  }
+
+  strings[count] = NULL;
+  showArray(strings);
+
+  for(int i = 0; i < count; i++){
+    free(strings[i]);
+  }
+  free(strings);
     return 0;
 }
+
+char *newString(char *str, int size){
+
+    char *newStr;
+    newStr = malloc(size * sizeof(char));
+    int index;
+
+    for(index = 0; index < size; index++){
+        newStr[index] = str[index];
+    }
+
+    return newStr;
+}
+
+void showArray(char **strings){
+
+    int i;
+    while(*strings != NULL){
+        
+        for(i = 0; i < strlen(*strings); i++){
+
+            printf("%c", (*strings)[i]);
+        }
+        strings++;
+    }
+}
+
